e7ac72eafc870b38c56baa2e4e4d0d3925335abe
All-Seher <153194916+All-Seher@users.noreply.github.com>
2024-03-20 17:11:57 +0500

    Update UserService.java

--- src/main/java/springboot/service/UserService.java
+++ src/main/java/springboot/service/UserService.java
@@ -15,7 +15,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Autowired
-    public UserSrvice(UserRepository userRepository) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = usereRepository;
     }
 
