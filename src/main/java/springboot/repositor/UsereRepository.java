f8b88b7ec12e97b742176705b08ef46d937ee1d0
All-Seher <153194916+All-Seher@users.noreply.github.com>
2024-03-20 16:34:45 +0500

    Update and rename UsereRepository.java to UserRepository.java

--- src/main/java/springboot/repositor/UsereRepository.java
+++
@@ -1,7 +0,0 @@
-package springboot.repositor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springboot.entity.User;
-
-public interface UsereRepository extends JpaRepository<User, Integer> {
-}
